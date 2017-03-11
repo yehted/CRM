64bcc18a01c59f72ad2c0aaec93501e528404cd8
Ted Yeh <yeh.ted@gmail.com>
2017-03-11 05:04:58 +0000

    set up db configs and structure

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-DEBUG = True
+class Config(object):
+    """ Common configurations across all environments """
+
+class DevConfig(Config):
+    """ Dev Environment configuration """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProdConfig(Config):
+    """ Prod Environment configuration """
+    DEBUG = False
+
+app_config = {
+    'dev': DevConfig,
+    'prod': ProdConfig }
