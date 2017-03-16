88afcd11c045a39b8b952f81be8daeaa1465bfa1
Ted Yeh <yeh.ted@gmail.com>
2017-03-16 05:00:18 +0000

    added images and fixed bugs

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
-    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(home_blueprint)
 
     return app
