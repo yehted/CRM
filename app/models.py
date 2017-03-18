96c17156f673f8bcf1b018d5130a95c005d578a9
Ted Yeh <yeh.ted@gmail.com>
2017-03-18 20:40:53 +0000

    added department and role model

--- app/models.py
+++ app/models.py
@@ -77,3 +77,33 @@ class Contact(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
 
     # projects
+
+
+class Department(db.Model):
+    """ Create a Deaprtment table """
+
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='department',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
+
+
+class Role(db.Model):
+    """ Create a Role table """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
