ee582f2993ba174aa94381fd5685ca81dbe4da51
Ted Yeh <yeh.ted@gmail.com>
2017-03-19 04:03:13 +0000

    add department view

--- app/admin/views.py
+++ app/admin/views.py
@@ -54,3 +54,34 @@ def edit_department(id):
     check_admin()
 
     add_department=False
+
+    department = Department.query.get_or_404(id)
+    form = DepartmentForm(obj=department)
+    if form.validate_on_submit():
+        department.name = form.name.data
+        department.description = form.description.data
+        db.session.commit()
+        flash('You have successfully edited the department.')
+
+        return redirect(url_for('admin.list_departments'))
+
+    form.description.data = department.description
+    form.name.data = department.name
+    return render_template('admin/departments/department.html', action="Edit",
+                            add_department=add_department, form=form,
+                            department=department, title="Edit Department")
+
+@admin.route('/departments/delete/<int:id>, methods=['GET', 'POST'])
+@login_required
+def delete_department(id):
+    """ Delete a department from the database """
+    check_admin()
+
+    department = Department.query.get_or_404(id)
+    db.session.delete(department)
+    db.session.commit()
+    flash('You have successfully deleted the department.')
+
+    return redirect(url_for('admin.list_departments'))
+
+    # return render_template(title="Delete Department")
